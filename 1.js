7aea4a76ac21626f48cdb9ae8f38a5aedc83d148
obedmwaanga <obedmwaanga2@gmail.com>
2023-07-25 12:29:17 +0200

    better code

--- 1.js
+++ 1.js
@@ -1,31 +1,8 @@
-function calculateTriangleArea(base, height) {
-    if (base <= 0 || height <= 0) {
-      throw new Error("Both base and height must be positive numbers.");
-    }
-  
-    return 0.5 * base * height;
-  }
-  
-  function promptNumber(message) {
-    let input;
-    do {
-      input = parseFloat(prompt(message));
-    } while (isNaN(input) || input <= 0);
-    return input;
-  }
-  
-  function main() {
-    const base = promptNumber("Enter the base length of the triangle: ");
-    const height = promptNumber("Enter the height of the triangle: ");
-  
-    try {
-      const area = calculateTriangleArea(base, height);
-      console.log("The area of the triangle is:", area);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-  
-  // Call the main function to start the program
-  main();
-  
+//first challenge
+
+var base = prompt('enter value of base');
+var height = prompt('enter value of height');
+
+  var answer = 0.5 * base * height;
+ 
+console.log('the area is ' + answer);
